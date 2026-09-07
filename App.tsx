5cb2ceb403c8f1ff2df5ab5a6b195da1be266fe3
Ona Bonnin <obonnin@uoc.edu>
2026-09-07 16:48:54 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,19 +2,68 @@ import React from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Dimensions
 } from 'react-native';
 
-const styles = Stylesheet.create({
+const styles = StyleSheet.create({
+  container: {
+    /*justifyContent: 'flex-end',
+    height: '120',
+    alignItems: 'flex-start',*/
+    backgroundColor: '#403728',
+    width: '100%',
+    padding: 20,
+    marginTop: 40,
+  }, 
   title: {
-    
+    fontSize: 20,
+    color: '#FFF',
+  },
+  text: {
+    fontSize: 16,
+    color: '#403728',
+  },
+  whitetext: {
+    fontSize: 16,
+    color: '#FFF',
+  },
+  textinput: {
+    borderColor: '#6f6f6f',
+    borderWidth: 1,
+    backgroundColor: '#EFE5D8',
+    width: Dimensions.get('screen').width * 0.6,
+    height: 30,
+    borderRadius: 3,
+    paddingLeft: 10,
+  },
+  inputcontainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+  },
+  addbutton: {
+    backgroundColor: '#D3B792',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('screen').width * 0.2,
+    height: 30,
+    borderRadius: 3,
   }
-})
+});
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Tasks</Text>
+      <View style={styles.inputcontainer}>
+        <TextInput style={styles.textinput}/>
+        <TouchableOpacity style={styles.addbutton}>
+          <Text style={styles.whitetext}>Add</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+    
   );
 }
