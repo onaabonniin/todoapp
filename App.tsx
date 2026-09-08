69fcf7f15f29f9461d8301cc3e506c8239a66ea2
Ona Bonnin <obonnin@uoc.edu>
2026-09-08 17:16:29 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,13 +5,14 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
-  Dimensions
+  Dimensions,
+  FlatList
 } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     /*justifyContent: 'flex-end',
-    height: '120',
+    height: 120,
     alignItems: 'flex-start',*/
     backgroundColor: '#403728',
     width: '100%',
@@ -30,18 +31,24 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#FFF',
   },
+  donetext: {
+    fontSize: 16,
+    color: '#FFF',
+    textDecorationLine: 'line-through',
+  },
   textinput: {
     borderColor: '#6f6f6f',
     borderWidth: 1,
     backgroundColor: '#EFE5D8',
-    width: Dimensions.get('screen').width * 0.6,
+    width: Dimensions.get('screen').width * 0.65,
     height: 30,
-    borderRadius: 3,
+    borderRadius: 4,
     paddingLeft: 10,
   },
   inputcontainer: {
     marginTop: 20,
     flexDirection: 'row',
+    justifyContent: 'space-between'
   },
   addbutton: {
     backgroundColor: '#D3B792',
@@ -50,18 +57,63 @@ const styles = StyleSheet.create({
     width: Dimensions.get('screen').width * 0.2,
     height: 30,
     borderRadius: 3,
+  },
+  scrollcontainer: {
+    
+  },
+  itemcontainer: {
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#EFE5D8',
   }
 });
 
+const tasks = [
+  {
+    title: 'Feed the dog',
+    done: false,
+    date: new Date(),
+  },
+  {
+    title: 'Do the laundry',
+    done: false,
+    date: new Date(),
+  },
+  {
+    title: 'New task',
+    done: true,
+    date: new Date(),
+  }
+];
+
+interface Task {
+  title: string;
+  done: boolean;
+  date: Date;
+}
+
 export default function App() {
+  function renderItem({item}: {item:Task}) {
+    return (
+      <View style={styles.itemcontainer}>
+        <TouchableOpacity>
+          <Text style={item.done ? styles.donetext : styles.whitetext}>{item.title}</Text>
+          <Text style={item.done ? styles.donetext : styles.whitetext}>Due date: {item.date.toLocaleDateString()}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My Tasks</Text>
       <View style={styles.inputcontainer}>
-        <TextInput style={styles.textinput}/>
+        <TextInput placeholder='Create a new task' style={styles.textinput}/>
         <TouchableOpacity style={styles.addbutton}>
           <Text style={styles.whitetext}>Add</Text>
         </TouchableOpacity>
+      </View>
+      <View style={styles.scrollcontainer}>
+        <FlatList renderItem={renderItem} data={tasks}/>
       </View>
     </View>
     
